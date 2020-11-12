ed42f22d6dffb8e60efa6f37b20d25174273ae25
Ostap Shutenko <ostapshutenko@gmail.com>
2020-11-13 02:26:28 +0300

    first

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ import requests
 #access_token=a3fed3eec848e1521eda0de817341d85700d20492dcccb96d9fc5ffee9a974753cad67955c5a6e1c9d3f6
 # &expires_in=86400
 # &user_id=155984790
-
-
 #id приложения - 7660892
 #sKTyEeniLq6BMpNUIX46
 #075661a6075661a6075661a695072284fa00756075661a658fbcfd9f89357ab4ea20157
@@ -21,7 +19,7 @@ import requests
 #https://oauth.vk.com/authorize?client_id=5490057&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.52
 #https://api.vk.com/method/friends.getOnline?v=5.52&access_token=
 #params_auth = {}
-
+#https://api.vk.com/method/friends.getOnline?v=5.52&access_token=
 link = 'https://api.vk.com/method/'
 command = 'groups.get'
 command2 = 'groups.getById'
